bc0a2cd4602ec592160258ef752d6906f387365c
Jasper Keijzer <jasperkeijzer96@gmail.com>
2020-04-10 10:16:21 +0200

    Initial commit

---
+++ tetris.py
@@ -0,0 +1,28 @@
+# -------------------------------
+# Name: Idle clicker
+# Author: Jasper Keijzer
+# 
+# Created: 10/04/2020
+# Inspiration & source for large amount of code:
+# TigerhawkT3
+# https://github.com/TigerhawkT3/tetris
+# https://www.youtube.com/playlist?list=PLQ7bGgvf9FtGJV3P4gj1cWdBYacQo7bKz
+# -------------------------------
+
+
+
+
+import tkinter as tk
+import random
+import pygame as pg
+
+class Tetris():
+	def __init__(self, parent):
+		pass
+
+
+
+
+root = tk.Tk()
+tetris = Tetris(root)
+root.mainloop()
